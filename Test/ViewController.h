33dafd067bac23ca858641fc9f6eb7c738237a2f
Chris Hsiao <chrishsiao1020@gmail.com>
2018-08-17 09:51:23 +0800

    first commit

---
+++ Test/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Winplus on 2018/8/3.
+//  Copyright © 2018年 Winplus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    int sum;
+}
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Winplus on 2018/8/3.
+//  Copyright © 2018年 Winplus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    int abc;
+}
+@end
+
+@implementation ViewController
+
+- (void)addFunctionA:(int) a andB:(int) b
+{
+    sum = a + b;
+    abc = 200;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    sum = 10;
+    abc = 100;
+    NSLog(@"abc = %d", abc);
+    [self addFunctionA:1 andB:1];
+    NSLog(@"sum = %d", sum);
+    NSLog(@"abc = %d", abc);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
