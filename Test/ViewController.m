35102d57e7c173f9004884b8b18100f9d4bc8066
Chris Hsiao <chrishsiao1020@gmail.com>
2018-08-17 10:11:12 +0800

    v1.0.0 commit

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -25,6 +25,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hello World");
+    
     sum = 10;
     abc = 100;
     NSLog(@"abc = %d", abc);
